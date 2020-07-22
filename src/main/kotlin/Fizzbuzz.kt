cc4d6cbe403f084501c2852517436ffd8908a200
Rebecca Williams <rebeccawilliams9941@gmail.com>
2020-07-22 18:32:46 +1000

    :recycle: Extract method chain out into accumulator string.

--- src/main/kotlin/Fizzbuzz.kt
+++ src/main/kotlin/Fizzbuzz.kt
@@ -1,18 +1,21 @@
 class Fizzbuzz(private val num: Int = 0) {
 
+    var result = ""
+
     fun divisibleBy3(): String {
-        if (num % 3 == 0) { return "Fizz" }
-        return divisibleBy5()
+        if (num % 3 == 0) { result += "Fizz" }
+        return result
     }
 
     fun divisibleBy5(): String {
-        if(num % 5 == 0) { return "Buzz" }
-        return notDivisible()
+        if(num % 5 == 0) { result += "Buzz" }
+        return result
     }
 
     fun divisibleBy3And5(): String {
-        if( "${divisibleBy3()}${divisibleBy5()}" == "FizzBuzz") { return "FizzBuzz" }
-        return divisibleBy3()
+        divisibleBy3()
+        divisibleBy5()
+        return result
     }
 
     fun notDivisible(): String {
@@ -20,7 +23,9 @@ class Fizzbuzz(private val num: Int = 0) {
     }
 
     fun fizzBuzz(): String {
-        return divisibleBy3And5()
+        divisibleBy3And5()
+        if(result != "") { return result }
+        return notDivisible()
     }
 
 }
