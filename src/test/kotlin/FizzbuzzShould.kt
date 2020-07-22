f40a7d9b470df76eb2bfca0472719cc23408b1a0
Rebecca Williams <rebeccawilliams9941@gmail.com>
2020-07-22 14:48:24 +1000

    :white_check_mark: Returns 1 when fizzbuzz is given 1.

--- src/test/kotlin/FizzbuzzShould.kt
+++ src/test/kotlin/FizzbuzzShould.kt
@@ -1,5 +1,6 @@
-import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.IsEqual.equalTo
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
 import org.junit.Test
 import org.junit.Ignore
 
@@ -7,5 +8,12 @@ class FizzbuzzShould {
     @Ignore @Test
     fun pass_simple_setup_test() {
         assertThat(true, equalTo(true))
+    }
+    @Test
+    fun doNothingWhenNotDivisibleBy3Or5() {
+        fun fizzbuzz(num: Int): String {
+          return "1"
+        }
+        assertEquals("1", fizzbuzz(1))
     }
 }
