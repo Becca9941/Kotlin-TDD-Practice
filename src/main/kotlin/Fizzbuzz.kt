3777ab73f1fdddf94fb0b2818e727819310b60d9
Rebecca Williams <rebeccawilliams9941@gmail.com>
2020-07-22 18:15:58 +1000

    :rocket: "Fizz" x 3, "Buzz" x 5, "FizzBuzz" x 3 and 5.

--- src/main/kotlin/Fizzbuzz.kt
+++ src/main/kotlin/Fizzbuzz.kt
@@ -1,16 +1,26 @@
 class Fizzbuzz(private val num: Int = 0) {
 
-    fun result(): String {
-       return divisibleBy3()
-    }
-
     fun divisibleBy3(): String {
         if (num % 3 == 0) { return "Fizz" }
+        return divisibleBy5()
+    }
+
+    fun divisibleBy5(): String {
+        if(num % 5 == 0) { return "Buzz" }
         return notDivisible()
     }
 
-    fun notDivisibleBy3(): String {
+    fun divisibleBy3And5(): String {
+        if( "${divisibleBy3()}${divisibleBy5()}" == "FizzBuzz") { return "FizzBuzz" }
+        return divisibleBy3()
+    }
+
+    fun notDivisible(): String {
         return num.toString()
+    }
+
+    fun fizzBuzz(): String {
+        return divisibleBy3And5()
     }
 
 }
--- src/test/kotlin/FizzbuzzShould.kt
+++ src/test/kotlin/FizzbuzzShould.kt
@@ -10,10 +10,10 @@ class FizzbuzzShould {
         assertThat(true, equalTo(true))
     }
     @Test
-    fun notDivisibleBy3() {
-        assertEquals("1", Fizzbuzz(1).notDivisibleBy3())
-        assertEquals("2", Fizzbuzz(2).notDivisibleBy3())
-        assertEquals("4", Fizzbuzz(4).notDivisibleBy3())
+    fun notDivisibleBy3Or5() {
+        assertEquals("1", Fizzbuzz(1).notDivisible())
+        assertEquals("2", Fizzbuzz(2).notDivisible())
+        assertEquals("4", Fizzbuzz(4).notDivisible())
 
     }
     @Test
@@ -21,5 +21,23 @@ class FizzbuzzShould {
         assertEquals("Fizz", Fizzbuzz(3).divisibleBy3())
         assertEquals("Fizz", Fizzbuzz(6).divisibleBy3())
         assertEquals("Fizz", Fizzbuzz(12).divisibleBy3())
+    }
+    @Test
+    fun divisibleBy5() {
+        assertEquals("Buzz", Fizzbuzz(5).divisibleBy5())
+        assertEquals("Buzz", Fizzbuzz(10).divisibleBy5())
+        assertEquals("Buzz", Fizzbuzz(20).divisibleBy5())
+    }
+    @Test
+    fun divisibleBy3And5() {
+        assertEquals("FizzBuzz", Fizzbuzz(15).divisibleBy3And5())
+        assertEquals("FizzBuzz", Fizzbuzz(30).divisibleBy3And5())
+        assertEquals("FizzBuzz", Fizzbuzz(60).divisibleBy3And5())
+    }
+    @Test
+    fun fizzBuzz() {
+        assertEquals("1", Fizzbuzz(1).fizzBuzz())
+        assertEquals("Fizz", Fizzbuzz(3).fizzBuzz())
+        assertEquals("FizzBuzz", Fizzbuzz(15).fizzBuzz())
     }
 }
