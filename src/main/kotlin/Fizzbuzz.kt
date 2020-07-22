b154078e8bc8ae79eae0c740d7954207b4cb91f8
Rebecca Williams <rebeccawilliams9941@gmail.com>
2020-07-22 16:34:26 +1000

    :recycle: Refactor to split behaviour into two methods.

--- src/main/kotlin/Fizzbuzz.kt
+++ src/main/kotlin/Fizzbuzz.kt
@@ -1,5 +1,13 @@
 fun fizzbuzz(num: Int): String {
-    if (num == 3) { return "Fizz" }
+    return divisibleBy3(num)
+}
+
+fun divisibleBy3(num: Int): String {
+    if (num % 3 == 0) { return "Fizz" }
+    return notDivisible(num)
+}
+
+fun notDivisible(num: Int): String {
     return num.toString()
 }
 
