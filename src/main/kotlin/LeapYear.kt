132f2f8daa8ae98a41464d415d92442161c0b392
Rebecca Williams <rebeccawilliams9941@gmail.com>
2020-07-23 15:32:44 +1000

    :white_check_mark: Not a leap year if divisible by 100.

--- src/main/kotlin/LeapYear.kt
+++ src/main/kotlin/LeapYear.kt
@@ -1,4 +1,5 @@
 fun checkIfLeapYear(year: Int): Boolean {
+    if(year % 100 == 0) { return false }
     if(year % 4 == 0) { return true }
     return false
 }
--- src/test/kotlin/LeapYearShould.kt
+++ src/test/kotlin/LeapYearShould.kt
@@ -4,8 +4,12 @@ import org.junit.Test
 class LeapYearShould {
     @Test
     fun divisibleByFour() {
-        mapOf(2000 to true, 2001 to false, 2004 to true).entries.forEach {
+        mapOf(2020 to true, 2001 to false, 2004 to true).entries.forEach {
             assertEquals(it.value, checkIfLeapYear(it.key))
         }
+    }
+    @Test
+    fun divisibleBy100() {
+        assertEquals(false, checkIfLeapYear(2100))
     }
 }
