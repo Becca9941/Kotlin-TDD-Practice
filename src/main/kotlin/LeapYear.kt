0096a15adeb48ddbd7eda3424bd2d03c1f5423f7
Rebecca Williams <rebeccawilliams9941@gmail.com>
2020-07-23 15:47:12 +1000

    :recycle: Extract behaviours into their own methods.

--- src/main/kotlin/LeapYear.kt
+++ src/main/kotlin/LeapYear.kt
@@ -1,5 +1,14 @@
 fun checkIfLeapYear(year: Int): Boolean {
-    if(year % 100 == 0) { return false }
+    if(divisibleBy4(year)) { return !divisibleBy100(year) }
+    return false
+}
+
+private fun divisibleBy4(year: Int): Boolean {
     if(year % 4 == 0) { return true }
+    return false
+}
+
+private fun divisibleBy100(year: Int): Boolean {
+    if(year % 100 == 0) { return true }
     return false
 }
