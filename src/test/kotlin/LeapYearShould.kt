cb5402cdbf3f180474d9adb265724f4d7d9baf4f
Rebecca Williams <rebeccawilliams9941@gmail.com>
2020-07-23 16:16:20 +1000

    :sparkles: Complete test-driven leap year kata woo woo!

--- src/test/kotlin/LeapYearShould.kt
+++ src/test/kotlin/LeapYearShould.kt
@@ -3,19 +3,34 @@ import org.junit.Test
 
 class LeapYearShould {
     @Test
-    fun divisibleByFour() {
+    fun beDivisibleByFour() {
         mapOf(2020 to true, 2001 to false, 2004 to true).entries.forEach {
             assertEquals(it.value, checkIfLeapYear(it.key))
         }
     }
     @Test
-    fun divisibleBy100() {
+    fun andNotDivisibleBy100Except() {
         arrayOf(2100, 2200, 2300).forEach {
             assertEquals(false, checkIfLeapYear(it))
         }
     }
     @Test
-    fun divisibleBy400() {
-        assertEquals(true, checkIfLeapYear(2000))
+    fun whenAlsoDivisibleBy400() {
+        arrayOf(2000, 2400, 2800).forEach {
+            assertEquals(true, checkIfLeapYear(it))
+        }
+    }
+    @Test
+    fun checkIfLeapYear() {
+        arrayOf(2000, 2004, 2008, 2012, 2016, 2020, 2024,
+                2028, 2032, 2036, 2040, 2044, 2048).forEach {
+            assertEquals(true, checkIfLeapYear(it))
+        }
+
+        arrayOf(1700, 1800, 1900, 2100, 2200, 2300, 2001,
+                2029, 2031, 2038, 2043, 2045, 2049).forEach {
+            assertEquals(false, checkIfLeapYear(it))
+        }
+
     }
 }
